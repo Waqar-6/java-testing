78248dbc560f0bec8831afe595a62745fe912b47
Waqar <waqar-1@hotmail.co.uk>
2024-12-14 20:58:06 +0000

    refactord invegeter division test to use the aaa pattern arrange act assert

--- junitbasics/src/main/java/Calculator.java
+++ junitbasics/src/main/java/Calculator.java
@@ -1,4 +1,7 @@
 public class Calculator {
 
     public int integerDivision (int dividend, int divisor) {return dividend/divisor;}
+    public int integerSubtraction(int minuend, int subtrahend) {
+        return minuend - subtrahend;
+    }
 }
--- junitbasics/src/test/java/CalculatorTest.java
+++ junitbasics/src/test/java/CalculatorTest.java
@@ -1,16 +1,57 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
+@DisplayName("Test Math Operations in Calculator class")
 public class CalculatorTest {
 
+    // pattern AAA
+
+    // Arrange
+
+    // Act
+
+    // Assert
+
+
+
+    // naming tests
+    // test<System Under Test>_< Condition or State Change>_
+
+    @DisplayName("Test 4/2 = 2")
     @Test
-    void integerDivision() {
+    void testIntegerDivision_WhenFourIsDividedByTwo_ShouldReturnTwo() {
+        // Arrange
+        // in this arrange section you initialise all the needed variables and objects needed for the test
         Calculator calculator = new Calculator();
-        int result = calculator.integerDivision(4,2);
+        int dividend = 4;
+        int divisor = 2;
+        int expectedResult = 2;
 
-        // verifying if result is two
-        assertEquals(2, result);
+        // Act
+        // in act section this is where you will invoke the method and test
+        int actualResult = calculator.integerDivision(dividend,divisor);
 
+        // Assert
+        // in assert section you will validate the test results
+        assertEquals(expectedResult, actualResult, "4/2 did not produce 2");
+
+    }
+
+    @DisplayName("Division by Zero")
+    @Test
+    void testIntegerDivision_WhenDividendIsDividedByZero_ShouldThrowArithmeticException() {
+        fail("Not implemented yet");
+    }
+    @Test
+    void integerSubtraction () {
+        Calculator calculator = new Calculator();
+        int minuend = 33;
+        int subtrahend = 1;
+        int expectedResult = 32;
+        int actualResult = calculator.integerSubtraction(minuend,subtrahend);
+        assertEquals(expectedResult, actualResult, () -> minuend + " " + subtrahend + " did not produce : " + expectedResult);
     }
 }
